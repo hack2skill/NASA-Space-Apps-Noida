4f41e0dd485b9880d8ada55de78fbcf4675d23c5
Vedanshi-Arya <142818538+Vedanshi-Arya@users.noreply.github.com>
2023-10-08 06:41:44 +0530

    Add files via upload
    committed

---
+++ quiz.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What percentage of the Earth's surface is covered by water?",
+    answer: "90%",
+    options: [
+      "50%",
+      "75%",
+      "90%",
+      "25%"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which process describes the conversion of water vapor into liquid water droplets, forming clouds?",
+    answer: "C",
+    options: [
+      "Condensation",
+      "Evaporation",
+      "Precipitation",
+      "Infiltration"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the primary source of freshwater on Earth?",
+    answer: "Groundwater ",
+    options: [
+      "Rivers",
+      "Lakes",
+      "Oceans",
+      "Groundwater"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is NOT a major use of water in households?",
+    answer: "Charging electronic devices",
+    options: [
+      "Drinking",
+      "Showering",
+      "Watering lawns",
+      "Charging electronics devices"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the term for the natural replenishment of groundwater by percolating rainwater through the soil?",
+    answer: "Recharge",
+    options: [
+      "Erosion",
+      "Evaporation",
+      "Transpiration",
+      "Recharge"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
